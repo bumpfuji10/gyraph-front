d92a652602df0665292a1b5fb367a26395522c8b
Yo Kamada <bumpfuji10@gmail.com>
2023-12-13 22:35:38 +0900

    ちょっとした変更

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,62 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} - {{ user.email }}
+      </li>
+      <li v-for="practiceRecord in practiceRecords" :key="practiceRecord.id">
+        {{ practiceRecord.id }} - {{ practiceRecord.practiced_date }}
+      </li>
+    </ul>
+  </div>
+</template>
+
 <script lang="ts">
-import axios from 'axios';
+import { getUsers } from '../resources/user';
+import { getPracticeRecords } from '../resources/practiceRecord'
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+interface PracticeRecord {
+  id: number;
+  practiced_date: Date;
+}
 
 export default {
+  data() {
+    return {
+      users: [] as User[],
+      practiceRecords: [] as PracticeRecord[]
+    }
+  },
   created() {
-    this.fetchPing();
+    this.fetchUsers();
+    this.fetchPracticeRecord();
   },
   methods: {
-    fetchPing() {
-      axios.get('http://localhost:3000/api/v1/ping')
-        .then(response => {
-          console.log('Response:', response.data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+    async fetchUsers(): Promise<void> {
+      try {
+        const response = await getUsers();
+        this.users = response.data
+        console.log(this.users);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async fetchPracticeRecord(): Promise<void> {
+      try {
+        const response = await getPracticeRecords();
+        this.practiceRecords = response.data
+        console.log(this.practiceRecords)
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
 }
+
 </script>
