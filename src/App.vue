205e0ed16996f4277693e1f24e7d09b6c12509d3
Yo <bumpfuji10@gmail.com>
2023-12-16 01:54:07 +0900

    Revert "ボタンをおもに作ったよ"

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
+<script setup lang="ts">
+import Home from './components/Home.vue'
+</script>
+
 <template>
   <div>
-    <NotLoginHeader></NotLoginHeader>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
     <Home></Home>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script setup lang="ts">
-import Home from './components/pages/PracticeRecordIndex.vue'
-import NotLoginHeader from './components/NotLoginHeader.vue'
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
