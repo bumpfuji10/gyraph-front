b5b25a2ab44a18122180b80d29fcdee910d03fb2
Yo Kamada <bumpfuji10@gmail.com>
2023-12-19 23:02:43 +0900

    router及びscssをapp.vue二設定

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import './styles/base.scss';
 import './styles/header.scss';
 import App from './App.vue'
+import router from '../router'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
