507c523b7eb43ab8ba2fd584c1c2393b138d1338
Yo Kamada <bumpfuji10@gmail.com>
2023-12-20 22:49:30 +0900

    ログイン画面のUIを途中まで

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>
-    <NotLoginHeader />
-    <div class="base-background">
-
+  <div class="base-background">
+    <div class="login-header-container">
+      <h1 class="baseCenterLogo">
+        <router-link to="/">Gyraph</router-link>
+      </h1>
     </div>
+    <div class="baseCenterText">
+      ログイン
+    </div>
+    <main class="base-main-zone">
+      <div class="login-grid">
+        <label for="" class="login-label">
+          メールアドレス
+        </label>
+        <form action="">
+          <input type="text" class="login-form-input">
+        </form>
+      </div>
+      <div class="login-grid">
+        <label for="" class="login-label">
+          パスワード
+        </label>
+        <form action="">
+          <input type="text" class="login-form-input">
+        </form>
+      </div>
+      <button class="login-button">
+        ログイン
+      </button>
+      <div>
+        <router-link to="/signup" class="redirectSignup">登録はこちら</router-link>
+      </div>
+    </main>
   </div>
 </template>
 
