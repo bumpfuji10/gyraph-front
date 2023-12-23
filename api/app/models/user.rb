4e763337e7f2fbda6218ac54a186c478b56b94df
Yo Kamada <bumpfuji10@gmail.com>
2023-12-23 23:59:02 +0900

    change: usermodel

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
-  validates :password_digest, presence: true, length: { minimum: 8 }
   has_secure_password
 
   has_many :practice_records
